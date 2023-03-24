099f4236362fd904a32a92be08f7e7a8408b7eaf
Minbu5 <mike.malibu1986@gmail.com>
2023-03-24 09:53:03 +0200

    Zaidimo eiga ir zaidimo laukas

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 
 # todo 1. Leistų žaisti dviems žaidėjams (X ir O).
-# todo 2. Teisingai fiksuotų žaidėjo laimėjimą ir stabdytų žaidimą
-# Suformuoti zaidimo laukus
 
+# Suformuoti zaidimo laukus
 veiksmo_laukas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 laimejimo_skaiciai = ["123", "456", "789", "147", "258", "369", "159", "753"]
-X_laukai = []
-O_laukai = []
+X_laukai_laimejimui = []
+O_laukai_laimejimui = []
 
 print(veiksmo_laukas[7:10])
 print(veiksmo_laukas[4:7])
@@ -15,13 +14,20 @@ print(veiksmo_laukas[1:4])
 # Suformuoti ivedimo mechanzma
 for _ in range(9):
     X_indeksas = int(input("Iveskite norimo pazymeti 'X' zenklu lauko numeri: ")) # Zaidejas pasirenka langeli
-    X_laukai.append(str(X_indeksas))
+    X_laukai_laimejimui.append(str(X_indeksas))
+    veiksmo_laukas[X_indeksas] = "X"
+    print(veiksmo_laukas[7:10])
+    print(veiksmo_laukas[4:7])
+    print(veiksmo_laukas[1:4])
+
     O_indeksas = int(input("Iveskite norimo pazymeti 'O' zenklu lauko numeri: ")) # Zaidejas pasirenka langeli
-    O_laukai.append(str(O_indeksas))
+    O_laukai_laimejimui.append(str(O_indeksas))
+    veiksmo_laukas[O_indeksas] = "O"
+    print(veiksmo_laukas[7:10])
+    print(veiksmo_laukas[4:7])
+    print(veiksmo_laukas[1:4])
 
-
-
-
+# todo 2. Teisingai fiksuotų žaidėjo laimėjimą ir stabdytų žaidimą
 # todo  3. Žaidimas vyktų konsolėje, grafinio interfeiso nereikia (bet galima daryti, tada konsolės nebereikia)
 # todo  4. Sukurtą žaidimą paskelbti github repozitorijoje, nuorodą paskelbti teamsuose,
 #           tam skirtoje užduotyje (Assignments)

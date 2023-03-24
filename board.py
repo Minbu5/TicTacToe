44a2815c020b4c7c61b7696741755e8867c579e2
Minbu5 <mike.malibu1986@gmail.com>
2023-03-24 15:06:15 +0200

    Nauja klase board

---
+++ board.py
@@ -0,0 +1,39 @@
+# todo 1. Leistų žaisti dviems žaidėjams (X ir O).
+class Lenta:
+    def __init__(self):
+        self.veiksmo_laukas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.laimejimimo_salygos = ["123", "456", "789", "147", "258", "369", "159", "753"]
+        self.max_ejimu = 0
+        self.zaidimas_vyksta = True
+
+    def veiksmo_lauko_vaizdas(self):
+        print(self.veiksmo_laukas[7:10])
+        print(self.veiksmo_laukas[4:7])
+        print(self.veiksmo_laukas[1:4])
+        # spauzdina 3 masyvus
+
+    def laimejimo_tikrinimas(self, ejimu_str):
+        '''
+        funkcija tikrina ar zaidejas turi susirinkes laimejimo kombinacija
+        (galima laimeti turint stringe bet kurios laimejimo kombinacijos
+        visus 3 skaicius) (Dvi istrizaines trys horizontales ir trys vertikales)
+        Grazina False, jeigu stringe yra laimejimo kombinacija.
+
+        :param ejimu_str: skaicius string tipo bet kokio ilgio
+        :return: Bolean
+        '''
+        for numeris in self.laimejimimo_salygos:
+            laimingas_skaicius = []
+            counter = 0
+            for skaiciukas in numeris:
+                laimingas_skaicius.append(skaiciukas)
+                if skaiciukas in ejimu_str:
+                    counter += 1
+            if counter == 3:
+                self.zaidimas_vyksta = False
+        return self.zaidimas_vyksta
+
+
+lenta = Lenta()
+lenta.veiksmo_lauko_vaizdas()
+print(lenta.laimejimo_tikrinimas("193"))
--- player.py
+++ player.py
@@ -1,13 +1,21 @@
 
 class Zaidejas:
     def __init__(self, zenklas='X'):
-        self zenklas = zenklas
+        self.zenklas = zenklas
+        self.laukai_laimejimui = []
+        self.ejimu_str = ""
+
+    def zaidejo_langelis(self):
+        indeksas = int(input("Iveskite norimo pazymeti 'X' zenklu lauko numeri: "))
+        # grazina numeriuka
+        self.laukai_laimejimui.append(str(indeksas))
+        self.veiksmo_laukas[indeksas] = "X"
+        self.ejimu_str += str(indeksas)
+    def laimejimo_laukai(self):
+        pass
 
 
 
-
-    # laukai_laimejimui = []
-    # ejimu_str = ""
-    #
-
+pl1 = Zaidejas()
+print(pl1.zaidejo_ejimas())
 

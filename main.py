e4947ae0a8665f6d114e61687bb10de3916913a2
Minbu5 <mike.malibu1986@gmail.com>
2023-03-27 14:43:33 +0300

    READY TO SUBMIT FOR BASE EVAL REQUIREMENTS

--- main.py
+++ main.py
@@ -3,40 +3,50 @@ from player import Player
 from colorama import Fore, Style
 from logo import x_win, o_win, draw
 
-board = Tboard()
-pl_1 = Player(f"{Fore.BLUE}X{Style.RESET_ALL}")
 
-pl_2 = Player(f"{Fore.YELLOW}O{Style.RESET_ALL}")
+def game():
+    board = Tboard()
 
-print(board.intro)
-while True:
-    # __________________P1___________________________
-    board.brd_displ()
-    while True:  # choice_validator will ask continuously for right input
-        move = pl_1.choice()
-        if board.choice_validator(move):
-            board.fld_kill(move, pl_1.symb)
-            pl_1.moves_reg(move)
-            break
-    if pl_1.win_check():  # winning check
-        print(x_win)
-        board.brd_displ()
-        break
+    pl_1 = Player(f"{Fore.BLUE}X{Style.RESET_ALL}")
+    pl_2 = Player(f"{Fore.YELLOW}O{Style.RESET_ALL}")
 
-    if board.draw():  # draw check
-        print(draw)
-        board.brd_displ()
-        break
+    print(board.intro)
 
-    # __________________P2___________________________
-    board.brd_displ()
     while True:
-        move = pl_2.choice()
-        if board.choice_validator(move):
-            board.fld_kill(move, pl_2.symb)
-            pl_2.moves_reg(move)
-            break
-    if pl_2.win_check():
-        print(o_win)
+        # __________________P1___________________________
         board.brd_displ()
-        break
+        while True:  # choice_validator will ask continuously for right input
+            move = pl_1.choice()
+            if board.choice_validator(move):
+                board.fld_kill(move, pl_1.symb)
+                pl_1.moves_reg(move)
+                break
+        if pl_1.win_check():  # winning check
+            print(x_win)
+            board.brd_displ()
+            break
+
+        if board.draw():  # draw check
+            print(draw)
+            board.brd_displ()
+            break
+
+        # __________________P2___________________________
+        board.brd_displ()
+        while True:
+            move = pl_2.choice()
+            if board.choice_validator(move):
+                board.fld_kill(move, pl_2.symb)
+                pl_2.moves_reg(move)
+                break
+        if pl_2.win_check():
+            print(o_win)
+            board.brd_displ()
+            break
+        #______________________Game again_________________
+
+    new_game = input("Jei norite žaisti dar kartą spauskite ENTER, jei ne - įveskite bet kokią raidę: ")
+    if len(new_game) < 1:
+        game()
+
+game()
